6d048986fda3e948c5fd08484c0a137fad3d1b65
RodzynDev <rodzyndawid@gmail.com>
2024-12-08 18:44:50 +0100

    Lab1 - Added missing mappedBy

--- wsb-jpa1/src/main/java/com/jpacourse/persistence/entity/PatientEntity.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistence/entity/PatientEntity.java
@@ -36,7 +36,7 @@ public class PatientEntity {
 	private AddressEntity address;
 
 	// Dwukierunkowa relacja
-	@OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
+	@OneToMany(mappedBy = "patient", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
 	private Collection<VisitEntity> visits;
 
 	public Long getId() {
